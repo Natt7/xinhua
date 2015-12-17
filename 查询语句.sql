cd4a87e2a8559348ec674227c7769ef427c6c4e3
Natt7 <natt.ni@qq.com>
2015-12-17 17:18:14 +0800

    添加查询语句

--- 查询语句.sql
+++ 查询语句.sql
@@ -4,3 +4,11 @@ select cgshdh,dgysid,gysmc,cwdlid,cwfl,rjfxid,rjflmc,sum(sssl),sum(ssmy),sum(sss
 select xsdh,khid,dwmc,case ykbz when 'true' then '越库' else (case zpfh when 'true' then '直配' else '报订' end) end,cwdlid,rjfxid,sum(sdsl),sum(sdmy),sum(sdsy),trunc(mdjsrq)from 1_2_result;
  
 select case ykbz when 'true' then '越库' else (case zpfh when 'true' then '直配' else '报订' end) end from 1_2_result;
+
+select cwdlid,
+       cwfl,
+       rjfxid,
+       rjflmc,
+       sum(xsmy),
+       count(spxxid),
+       trunc(xsrq) from 2_1_result;
