f08b920d73fb58896fa8cecf2da81a3a53925ec9
Natt7 <natt.ni@qq.com>
2015-12-28 22:12:07 +0800

    修改品类视图原始语句

--- 品类视图/品类视图.sql
+++ 品类视图/品类视图.sql
@@ -1,10 +1,22 @@
-CREATE VIEW jt_j_fxfl_rjfl AS
-SELECT dj.fxflid,
-       dj.fxflmc,
-       bj.fxflid rjfxid,
-       bj.fxflmc rjflmc,
-       bj.cwdlid
-FROM jt_j_fxfl dj
-LEFT JOIN jt_j_fxfl cj ON dj.fxflfid = cj.fxflid
-LEFT JOIN jt_j_fxfl bj ON cj.fxflfid = bj.fxflid
-WHERE dj.fxfljs = 4
+Create view jt_j_fxfl_rjfl as 
+select dj.fxflid,dj.fxflmc,bj.fxflid rjfxid,bj.fxflmc rjflmc,bj.cwdlid
+  from jt_j_fxfl dj
+  left join jt_j_fxfl cj
+    on dj.fxflfid = cj.fxflid
+  left join jt_j_fxfl bj
+    on cj.fxflfid = bj.fxflid
+  where dj.fxfljs = 4
+union all
+select dj.fxflid,dj.fxflmc,bj.fxflid rjfxid,bj.fxflmc rjflmc,bj.cwdlid
+  from jt_j_fxfl dj
+  left join jt_j_fxfl bj
+    on dj.fxflfid = bj.fxflid
+  where dj.fxfljs = 3
+union all
+select dj.fxflid,dj.fxflmc,dj.fxflid rjfxid,dj.fxflmc rjflmc,dj.cwdlid
+  from jt_j_fxfl dj
+  where dj.fxfljs = 2
+
+
+  select fxflid,fxflmc,rjfxid,rjflmc,cwdlid
+  from jt_j_fxfl_rjfl;
